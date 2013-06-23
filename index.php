de73f62c60b1d9f262b7e0297ba0563639c78b42
Ben Falconer <ben@falconers.me.uk>
2013-06-23 19:49:04 +0100

    merged ruth's changes

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 <?php
 $pagename = 'Campaigns';
 include('header.php');
-
-$link = mysql_connect('localhost', 'ruth', 'password');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_select_db("ruth") or die(mysql_error())
 ?>
-
-
-
-
-
-
-
 <p>Welcome to the campaign collaboration webspace. Please browse campaigns to vote and have your say or create your own campaign suggestion.</p>
-<p>Click <a href="login.html">here</a> to create a new account</p>
-
+<p>Click <a href="new_account.php">here</a> to create a new account</p>
 <?php
 $query = sprintf("SELECT * FROM OS_SfCs");
 
 // Perform Query
-$result = mysql_query($query);
+$result = $mysqli->query($query);
 
 // Check result
 // This shows the actual query sent to MySQL, and the error. Useful for debugging.
@@ -40,7 +25,7 @@ if (!$result) {
 // Attempting to print $result won't allow access to information in the resource
 // One of the mysql result functions must be used
 // See also mysql_result(), mysql_fetch_array(), mysql_fetch_row(), etc.
-while ($row = mysql_fetch_assoc($result)) {
+while ($row = $result->fetch_assoc()) {
     $name = $row['Name'];
     $ID = $row['ID'];
 
@@ -51,8 +36,5 @@ echo"</table>";
     
 // Free the resources associated with the result set
 // This is done automatically at the end of the script
-mysql_free_result($result);
-mysql_close($link);
-?>
-
-<?include('footer.php');
+$result->free_result();
+include('footer.php');
