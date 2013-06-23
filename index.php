d27c864d58ac7a781b9b4750a65abeca8d99d37d
Ruth King <ruth.king1991@gmail.com>
2013-06-23 18:18:39 +0100

    Added database to search for SfCs and display these on front page, clickable to SfC page

--- index.php
+++ index.php
@@ -1,19 +1,58 @@
 <?php
 $pagename = 'Campaigns';
 include('header.php');
-?>
-<p>Welcome to the campaign collaboration webspace. Please browse campaigns to vote and have your say or create your own campaign suggestion.</p>
 
+$link = mysql_connect('localhost', 'ruth', 'password');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_select_db("ruth") or die(mysql_error())
+?>
+
+
+
+
+
+
+
+<p>Welcome to the campaign collaboration webspace. Please browse campaigns to vote and have your say or create your own campaign suggestion.</p>
 <p>Click <a href="login.html">here</a> to create a new account</p>
 
-<table>
-<tr>
-  <td>LINK TO FIRST THING</td>
-  <td>x votes</td>
-</tr>
-<tr>
-  <td>LINK TO SECOND THING</td>
-  <td>y votes</td>
-</tr>
-</table>
+<?php
+$query = sprintf("SELECT * FROM OS_SfCs");
+
+// Perform Query
+$result = mysql_query($query);
+
+// Check result
+// This shows the actual query sent to MySQL, and the error. Useful for debugging.
+if (!$result) {
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+}
+
+ echo "<table>";
+  echo "<tr><th>Name</th></tr>";
+
+// Use result
+// Attempting to print $result won't allow access to information in the resource
+// One of the mysql result functions must be used
+// See also mysql_result(), mysql_fetch_array(), mysql_fetch_row(), etc.
+while ($row = mysql_fetch_assoc($result)) {
+    $name = $row['Name'];
+    $ID = $row['ID'];
+
+echo "<tr><td style='width: 200px;'><a href='sfc.php?id=".$ID."'>".$name."</a></td></tr>";
+}
+
+echo"</table>";
+    
+// Free the resources associated with the result set
+// This is done automatically at the end of the script
+mysql_free_result($result);
+mysql_close($link);
+?>
+
 <?include('footer.php');
--- sfc.php
+++ sfc.php
@@ -1,10 +1,46 @@
 <?php
 $pagename = 'SFCs';
 include('header.php');
-?>
-<h1>Some kind of SFC</h1>
-<button>Up vote</button>
-<button>Down vote</button>
+$ID = $_GET["id"];
 
-<p>Text about sfc</p>
+$link = mysql_connect('localhost', 'ruth', 'password');
+ if (!$link) {
+     die('Could not connect: ' . mysql_error());
+ } 
+echo 'Connected successfully';
+mysql_select_db("ruth") or die(mysql_error());
+
+$query = sprintf("SELECT * FROM OS_SfCs WHERE ID = '$ID'");
+
+// Perform Query
+$result = mysql_query($query);
+ 
+// Check result
+// This shows the actual query sent to MySQL, and the error. Useful for debu    gging.
+if (!$result) {
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+}
+
+
+// Use result
+// Attempting to print $result won't allow access to information in the reso    urce
+// One of the mysql result functions must be used
+// See also mysql_result(), mysql_fetch_array(), mysql_fetch_row(), etc.
+$row = mysql_fetch_assoc($result);
+$name = $row['Name'];
+$creator = $row['Creator'];
+$details = $row['Details'];
+
+echo "<h1>".$name."</h1>";
+echo "<p>".$details."</p>";
+
+// Free the resources associated with the result set
+// This is done automatically at the end of the script
+mysql_free_result($result);
+mysql_close($link);
+
+?>
+
 <?include('footer.php');
