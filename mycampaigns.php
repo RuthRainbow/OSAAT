0ccb240ae2ed7d58910ab2532d6c34cae6a563bd
Ben Falconer <ben@falconers.me.uk>
2013-06-23 23:22:07 +0100

    Added voted for to mycampaigns

--- mycampaigns.php
+++ mycampaigns.php
@@ -7,7 +7,7 @@ require_login();
 <table id="created">
 	<tr><th>Name</th></tr>
 <?
-$result = $mysqli->query('SELECT * FROM OS_SfCs WHERE Creator='.$login['id']);
+$result = $mysqli->query('SELECT * FROM '.$mysql_prefix.'SfCs WHERE Creator='.$login['id']);
 while($row = $result->fetch_assoc())
 {
 	echo('<tr><td><a href="sfc.php?id='.$row['ID'].'">'.$row['Name'].'</a></td></tr>');
@@ -16,5 +16,19 @@ while($row = $result->fetch_assoc())
 </table>
 
 <h2>Campaigns I voted for</h2>
+<table id="voted">
+	<tr><th>Name</th></tr>
+<?
+$result = $mysqli->query('SELECT * FROM '.$mysql_prefix.'UsersVotes WHERE UserID='.$login['id']);
+while($row = $result->fetch_assoc())
+{
+	$result2 = $mysqli->query('SELECT * FROM '.$mysql_prefix.'SfCs WHERE ID='.$row['SfCID']);
+	while($row2 = $result2->fetch_assoc())
+	{
+		echo('<tr><td><a href="sfc.php?id='.$row['SfCID'].'">'.$row2['Name'].'</a></td></tr>');
+	}
+}
+?>
+</table>
 <?
 include('footer.php');
