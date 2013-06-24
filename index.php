d46f75d898cf9929ba0d89c945f8965a26a415e7
Ben Falconer <ben@falconers.me.uk>
2013-06-24 05:51:01 +0100

    Added location aware campaign suggestions

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include('header.php');
 ?>
 <p>Welcome to the campaign collaboration webspace. Please browse campaigns to vote and have your say or create your own campaign suggestion.</p>
 <p>Click <a href="new_account.php">here</a> to create a new account</p>
-<?php
+<?
 
-$result = $mysqli->query("SELECT * FROM OS_SfCs");
+$result = $mysqli->query('SELECT * FROM '.$mysql_prefix.'SfCs ORDER BY NumVotes DESC');
 
 if (!$result) {
     $message  = 'Invalid query: ' . mysql_error() . "\n";
@@ -14,6 +14,7 @@ if (!$result) {
     die($message);
 }
 ?>
+<h2>Popular campaigns</h2>
 <table>
 <tr><th>Votes</th><th>Name</th></tr>
 <?
@@ -31,8 +32,66 @@ while ($row = $result->fetch_assoc()) {
 	</tr>
 <?
 }
+$result->free();
 ?>
 </table>
 <?
-$result->free_result();
+if(isset($login['id']))
+{
+	$result = $mysqli->query('SELECT * FROM '.$mysql_prefix.'Users WHERE ID='.$login['id']);
+	$row = $result->fetch_assoc();
+	$latitude = $row['Latitude'];
+	$longitude = $row['Longitude'];
+	$region = $row['Location'];
+	$result->free();
+} else {
+	//if user isn't logged on then get their location from their IP:
+	$geoip = json_decode(file_get_contents('http://freegeoip.net/json/'.$_SERVER['REMOTE_ADDR']));
+	$latitude = $mysqli->real_escape_string($geoip->latitude);
+	$longitude = $mysqli->real_escape_string($geoip->longitude);
+	$region = $mysqli->real_escape_string($geoip->region_name);
+}
+$mysqli->query('
+	CREATE FUNCTION `lat_lng_distance` (lat1 FLOAT, lng1 FLOAT, lat2 FLOAT, lng2 FLOAT)
+	RETURNS FLOAT
+	DETERMINISTIC
+	BEGIN
+	RETURN 6371 * 2 * ASIN(SQRT(
+		POWER(SIN((lat1 - abs(lat2)) * pi()/180 / 2),
+		2) + COS(lat1 * pi()/180 ) * COS(abs(lat2) *
+		pi()/180) * POWER(SIN((lng1 - lng2) *
+		pi()/180 / 2), 2) ));
+	END
+');
+if($result = $mysqli->query('SELECT NumVotes, Name, ID, lat_lng_distance('.$latitude.', '.$longitude.', Latitude, Longitude) AS Distance FROM '.$mysql_prefix.'SfCs ORDER BY Distance ASC'))
+{
+?>
+<h2>Campaigns near <?echo($region)?></h2>
+<table>
+<tr><th>Votes</th><th>Distance</th><th>Name</th></tr>
+<?
+	while ($row = $result->fetch_assoc()) {
+?>
+	<tr>
+		<td class="votes">
+			<?echo($row['NumVotes'])?>
+		</td>
+		<td class="distance">
+			<?echo(round($row['Distance']).'km')?>
+		</td>
+		<td class="campaign-name">
+			<a href="sfc.php?id=<?echo($row['ID'])?>">
+				<?echo(stripslashes($row['Name']))?>
+			</a>
+		</td>
+	</tr>
+<?
+	}
+	$result->free();
+?>
+</table>
+<?
+} else {
+	echo('something broke: '.$mysqli->error);
+}
 include('footer.php');
--- new_account.php
+++ new_account.php
@@ -63,10 +63,22 @@ if(isset($_POST['submit']))
 		array_push($errors, "Enter an expertise");
 	}
 
+	if(existsWithLength('location')) {
+		$location = $_POST['location'];
+	} else {
+		$error = true;
+		array_push($errors, "Enter your location");
+	}
+
 	if (!$error) {
+		//lookup location:
+		$geolookup = json_decode(file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($_POST['location']).'&sensor=false'));
+		$latitude = $geolookup->results[0]->geometry->location->lat;
+		$longitude = $geolookup->results[0]->geometry->location->lng;
+
 		$salt = hash('sha256', mcrypt_create_iv(20));
 		$passwordHash = hash('sha256', $salt.$password);
-		if ($result = $mysqli->query("INSERT INTO " . $mysql_prefix . "Users(Username, Password, Salt, FirstName, SecondName, Email, Age, Interests, Expertise) VALUES (
+		if ($result = $mysqli->query("INSERT INTO " . $mysql_prefix . "Users(Username, Password, Salt, FirstName, SecondName, Email, Age, Interests, Expertise, Location, Latitude, Longitude) VALUES (
 			'".$mysqli->real_escape_string($username)."',
 			'".$passwordHash."',
 			'".$salt."',
@@ -75,7 +87,10 @@ if(isset($_POST['submit']))
 			'".$mysqli->real_escape_string($email)."',
 			'".$mysqli->real_escape_string($age)."',
 			'".$mysqli->real_escape_string($interests)."',
-			'".$mysqli->real_escape_string($expertise)."')")) {
+			'".$mysqli->real_escape_string($expertise)."',
+			'".$mysqli->real_escape_string($location)."',
+			'".$mysqli->real_escape_string($latitude)."',
+			'".$mysqli->real_escape_string($longitude)."')")) {
 ?>
 		<p>Congratulations, you are now registered, you may now login</p>
 <?php
@@ -102,6 +117,7 @@ if (!isset($_POST['submit']) || $error) {
 	Age: <input type="number" name="age" value="<?php echo (isset($age) ? htmlspecialchars($age) : '') ?>" /><br>
 	Interests: <input type="text" name="interests" value="<?php echo (isset($interests) ? htmlspecialchars($interests) : '') ?>" /><br>
 	Expertise: <input type="text" name="expertise" value="<?php echo (isset($expertise) ? htmlspecialchars($expertise) : '') ?>" /><br>
+	Location: <input type="text" name="location" value="<?php echo (isset($location) ? htmlspecialchars($location) : '') ?>" /><br>
 
 	<input type="submit" value="Create" name="submit" />
 </form>
