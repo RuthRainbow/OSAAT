ea159fe81678f594dd02900ba91680ec35ad55e2
Ben Falconer <ben@falconers.me.uk>
2013-06-24 01:21:14 +0100

    Fixed orgs slightly

--- my_orgs.php
+++ my_orgs.php
@@ -17,13 +17,13 @@ while($row = $result->fetch_assoc()) {
 
 <h2>Organisations I administrate</h2>
 <table id="admin">
-        <tr><th>Name</th></tr>
+	<tr><th>Name</th></tr>
 <?
 $result = $mysqli->query('SELECT * FROM '.$mysql_prefix.'Admins WHERE UserID='.$login['id']);
 while($row = $result->fetch_assoc()) {
 	$result2 = $mysqli->query('SELECT * FROM '.$mysql_prefix.'Orgs WHERE ID='.$row['OrgID']);
 	while($row2 = $result2 -> fetch_assoc()) {
-        	echo('<tr><td><a href="org.php?id='.$row['ID'].'">'.stripslashes($row2['Name']).'</a></td></tr>');
+		echo('<tr><td><a href="org.php?id='.$row2['ID'].'">'.stripslashes($row2['Name']).'</a></td></tr>');
 	}
 }
 ?>
